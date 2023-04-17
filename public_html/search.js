eecda3af36ed4e5f34bcc1f8c7d460934d4d884d
Jacob <chambersjacobj@gmail.com>
2023-04-17 15:00:01 -0700

    Onload function for search

--- public_html/search.js
+++ public_html/search.js
@@ -102,3 +102,16 @@ function drawGraph(data, curStock) {
         },
   });
 }
+
+/*
+ * This will run the code to display the correct graph and information in the table
+ * when a user searches for a stock
+*/
+function pageLoad() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const search = urlParams.get('search');
+  graphInfo("week", search);
+
+  // TODO: Populate the table with the data from the API
+}
