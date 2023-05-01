e718b4b42e9eef83112da6afa2a8d0106b0cccbf
gurvirdhillon17 <130307317+gurvirdhillon17@users.noreply.github.com>
2023-05-01 15:03:15 -0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,40 @@
+const { MongoClient } = require('mongodb');
+
+// Connection URI for your MongoDB database
+const uri = "mongodb+srv://username:password@cluster.mongodb.net/database_name?retryWrites=true&w=majority";
+
+// Create a new MongoClient instance
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+/**
+ * Connect to the MongoDB database using the MongoClient instance
+ */
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("Connected to database");
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+/**
+ * Get a collection from the database
+ * @param {String} collectionName - The name of the collection to retrieve
+ * @return {Collection} - The MongoDB collection object
+ */
+async function getCollection(collectionName) {
+    try {
+        const db = client.db("your_database_name");
+        const collection = db.collection(collectionName);
+        return collection;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Export the connectDB and getCollection functions
+module.exports = {
+    connectDB,
+    getCollection,
+};
